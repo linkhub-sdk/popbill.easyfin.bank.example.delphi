f6d1c798053baacf4279152be665043918380bf2
ProWON <pro_won45@naver.com>
2022-01-12 09:52:24 +0900

    fixed commnet

--- Example.pas
+++ Example.pas
@@ -136,16 +136,16 @@ procedure TTfrmExample.FormCreate(Sender: TObject);
 begin
         easyFinBankService := TEasyFinBankService.Create(LinkID,SecretKey);
 
-        // 연동환경 설정값, true(개발용), false(상업용)
+        // 연동환경 설정, true-개발용, false-상업용
         easyFinBankService.IsTest := true;
 
-        // Exception 처리여부,  기본값(true)
-        easyFinBankService.IsThrowException := false;
+        // Exception 처리 설정, true-사용, false-미사용, 기본값(true)
+        easyFinBankService.IsThrowException := true;
 
-        // 인증토큰 IP제한기능 사용여부, true(권장)
+        // 인증토큰 IP제한기능 사용여부, true-사용, false-미사용, 기본값(true)
         easyFinBankService.IPRestrictOnOff := true;
 
-        //로컬시스템 시간 사용여부, true(사용), false(미사용) - 기본값
+        //로컬시스템 시간 사용여부, true-사용, false-미사용, 기본값(true)
         easyFinBankService.UseLocalTimeYN := false;
 
         StringGrid1.Cells[0,0] := 'tid';
